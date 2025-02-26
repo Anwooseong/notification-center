6a11c2ec4d958000e1699e20e138cae39c9f8651
Anwooseong <aws5624@naver.com>
2025-02-26 14:46:54 +0900

    chore: 멀티 모듈 의존성 정리

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,21 +1,5 @@
-plugins {
-    id("java")
-}
-
-group = "org.example"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     //core
-
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation(project(mapOf("path" to ":core")))
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
