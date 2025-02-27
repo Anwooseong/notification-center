0d9b5b8670586a2add4d2be472f51b0fba926e4a
Anwooseong <aws5624@naver.com>
2025-02-27 18:02:17 +0900

    feat: 팔로우 추가 이벤트 처리 구현

--- consumer/src/main/java/com/ws/event/FollowEventConsumer.java
+++ consumer/src/main/java/com/ws/event/FollowEventConsumer.java
@@ -1,7 +1,6 @@
 package com.ws.event;
 
 import com.ws.task.FollowAddTask;
-import com.ws.task.FollowRemoveTask;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -15,15 +14,12 @@ import java.util.function.Consumer;
 public class FollowEventConsumer {
 
     private final FollowAddTask followAddTask;
-    private final FollowRemoveTask followRemoveTask;
 
     @Bean(name = "follow")
     public Consumer<FollowEvent> like() {  //application-event.yml 에 등록된 function definition 에 따라서 함수명 설정
         return event -> {
             if (event.getType() == FollowEventType.ADD) {
                 followAddTask.processEvent(event);
-            } else if (event.getType() == FollowEventType.REMOVE) {
-                followRemoveTask.processEvent(event);
             }
         };
     }
